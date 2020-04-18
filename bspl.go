5fd403bde05d5098b8aea408919a310eb21a7ba7
mikelsr <mikelsr@pm.me>
2020-04-18 15:33:15 +0200

    Add type alias for proto structs

--- bspl.go
+++ bspl.go
@@ -7,8 +7,30 @@ import (
 	"github.com/mikelsr/bspl/proto"
 )
 
+type (
+	// Action is an alias for proto.Action
+	Action = proto.Action
+	// IO is an alias for proto.IO
+	IO = proto.IO
+	// Parameter is an alias for proto.Parameter
+	Parameter = proto.Parameter
+	// Protocol is an alias for proto.Protocol
+	Protocol = proto.Protocol
+	// Role is an alias for proto.Role
+	Role = proto.Role
+)
+
+const (
+	// In defines a local scope
+	In IO = proto.In
+	// Out defines a global scope
+	Out IO = proto.Out
+	// Nil defines a parameter missing from a protocol instance
+	Nil IO = proto.Nil
+)
+
 // Parse a BSPL protocol
-func Parse(in io.Reader) (proto.Protocol, error) {
+func Parse(in io.Reader) (Protocol, error) {
 	tokens, err := parser.LexStream(in)
 	if err != nil {
 		return proto.Protocol{}, err
